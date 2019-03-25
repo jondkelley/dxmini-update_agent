55e5ab8faffe3e502747a9c40c09306eeaea6a77
Jonathan Kelley <jkelley@boomtownroi.com>
2019-03-25 16:03:55 -0400

    Update upnp port struct

--- dxmini/agent.py
+++ dxmini/agent.py
@@ -184,23 +184,28 @@ def get_upnp_settings():
     """
     retrieves current upnp configurations
     """
-    upnp_enabled_cmd = """/bin/grep '$DAEMON -a' /usr/local/sbin/pistar-upnp.service  | /bin/grep -e '^#' | /usr/bin/awk '{ print "{ \"inside\": " $5 ", \"outside\": " $6 " \"proto\": \"" $7 "\" }"}'"""
-    upnp_disabled_cmd = """/bin/grep '$DAEMON -a' /usr/local/sbin/pistar-upnp.service  | /bin/grep -v -e '^#' | /usr/bin/awk '{ print "{ \"inside\": " $5 ", \"outside\": " $6 " \"proto\": \"" $7 "\" }"}'"""
+    upnp_enabled_cmd = """/bin/grep '$DAEMON -a' /usr/local/sbin/pistar-upnp.service | /bin/grep -v -e '^#' | /usr/bin/awk '{ print $4 " " $5 " "  $6}'"""
+    upnp_disabled_cmd = """/bin/grep '$DAEMON -a' /usr/local/sbin/pistar-upnp.service | /bin/grep -e '^#' | /usr/bin/awk '{ print $5 " " $6 " "  $7}'"""
     if os.path.isfile('/usr/local/sbin/pistar-upnp.service'):
-        enabled_upnp = subprocess.check_output(upnp_enabled_cmd, shell=True).decode('utf-8') ##.split('\n')[:-1]
-        disabled_upnp = subprocess.check_output(upnp_disabled_cmd, shell=True).decode('utf-8')##.split('\n')[:-1]
-        struct = { "on": {}, "off": {}}
-        i = 0
-        for rule in enabled_upnp:
-            i += 1
-            struct['on'][i] = json.loads(rule)
-        i = 0
-        for rule in disabled_upnp:
-            i += 1
-            struct['off'][i] = json.loads(rule)
-        return struct
+        enabled_upnp = subprocess.check_output(upnp_enabled_cmd, shell=True).decode('utf-8')
+        disabled_upnp = subprocess.check_output(upnp_disabled_cmd, shell=True).decode('utf-8')
+        enabled_ports = {'UDP': [], 'TCP': []}
+        disabled_ports = {'UDP': [], 'TCP': []}
+        for line in enabled_upnp.rstrip().split('\n'):
+            fields = line.split()
+            inside = fields[0]
+            outside = fields[1]
+            proto = fields[2]
+            enabled_ports[proto].append({"in": inside, "out": outside})
+        for line in disabled_upnp.rstrip().split('\n'):
+            fields = line.split()
+            inside = fields[0]
+            outside = fields[1]
+            proto = fields[2]
+            disabled_ports[proto].append({"in": inside, "out": outside})
+        return {"enabled": enabled_ports, "disabled": disabled_ports}
     else:
-        return False
+        return dict()
 
 def get_wpa_supplicant():
     """
