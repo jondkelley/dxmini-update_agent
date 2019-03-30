b35ca94ed2294ff35a04ef82a507653245553b44
Jonathan Kelley <jkelley@boomtownroi.com>
2019-03-29 20:32:18 -0400

    X

--- dxmini/lib/utils.py
+++ dxmini/lib/utils.py
@@ -33,16 +33,30 @@ def print_arguements(args):
     print(AnsiColor.red)
     print("{}{}".format(dash, AnsiColor.end))
 
+# class AnsiColor(object):
+#     """
+#     life is better in color
+#     """
+#     header = '\033[95m'
+#     blue = '\033[1;94m'
+#     green = '\033[1;92m'
+#     yellow = '\033[93m'
+#     red = '\033[91m'
+#     magenta = '\033[91m'
+#     end = '\033[0m'
+#     bold = '\033[1m'
+#     underline = '\033[4m'
+
 class AnsiColor(object):
     """
     life is better in color
     """
-    header = '\033[95m'
-    blue = '\033[1;94m'
-    green = '\033[1;92m'
-    yellow = '\033[93m'
-    red = '\033[91m'
-    magenta = '\033[91m'
-    end = '\033[0m'
-    bold = '\033[1m'
-    underline = '\033[4m'
+    header = ''
+    blue = ''
+    green = ''
+    yellow = ''
+    red = ''
+    magenta = ''
+    end = ''
+    bold = ''
+    underline = ''
